1ed3b44feb1225980a7ce08c43ac9623259a04f0
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2018-07-29 07:18:50 +0300

    minor fix

--- src/golem/viz.clj
+++ src/golem/viz.clj
@@ -6,7 +6,7 @@
   (let [n 0
         xs (for [x sm]
              (partition 2 (interleave (repeat (key x)) (keep :next-state (val x)))))]
-    (->> (reduce (fn [a b] (conj a (map (fn [x] (into [] (interpose (keyword (gensym)) x))) b))) '() xs)
+    (->> (reduce (fn [a b] (conj a (map (fn [x] (into [] (interpose (gensym) x))) b))) '() xs)
          (reduce into []))))
 
 (defn generate-image [xs name]
